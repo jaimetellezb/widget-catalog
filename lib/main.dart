3cb656b536ccc8944cfe243daf42fe53536ba972
jaimetellezb <jaimetellez410@gmail.com>
2024-02-03 11:40:14 -0500

    changes Theme widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,40 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+        colorScheme: const ColorScheme(
+          brightness: Brightness.dark,
+          primary: Color(0xFFb794f6),
+          onPrimary: Color.fromARGB(255, 162, 134, 176),
+          secondary: Color(0xFFc6f68d),
+          onSecondary: Color.fromARGB(255, 77, 40, 145),
+          error: Color(0xFFb794f6),
+          onError: Color(0xFFb794f6),
+          background: Color.fromARGB(255, 242, 241, 244),
+          onBackground: Color(0xFFb794f6),
+          surface: Color(0xFFb794f6),
+          onSurface: Color.fromARGB(255, 73, 71, 75),
+        ),
+        // https://m3.material.io/styles/typography/type-scale-tokens
+        textTheme: TextTheme(
+            displayLarge: const TextStyle(),
+            displayMedium:
+                const TextStyle(color: Color.fromARGB(255, 77, 40, 145)),
+            displaySmall: const TextStyle(),
+            headlineLarge: const TextStyle(),
+            headlineMedium: const TextStyle(),
+            headlineSmall: const TextStyle(),
+            titleLarge: const TextStyle(),
+            titleMedium: const TextStyle(),
+            titleSmall: const TextStyle(),
+            labelLarge: const TextStyle(),
+            labelMedium: const TextStyle(),
+            labelSmall: const TextStyle(),
+            bodyLarge: TextStyle(color: Theme.of(context).primaryColor),
+            bodyMedium: TextStyle(color: Theme.of(context).primaryColor),
+            bodySmall: const TextStyle()),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter'),
     );
   }
 }
@@ -40,21 +68,27 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+    final textTheme = Theme.of(context).textTheme;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text(
+          widget.title,
+          style: textTheme.titleLarge,
+        ),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
+            Text(
               'You have pushed the button this many times:',
+              style: textTheme.bodyMedium,
             ),
             Text(
               '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              style: textTheme.headlineMedium,
             ),
           ],
         ),
