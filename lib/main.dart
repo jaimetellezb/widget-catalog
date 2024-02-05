37231cb578f76be2232504cbe670a00e5e8a4dd1
jaimetellezb <jaimetellez410@gmail.com>
2024-02-04 22:35:34 -0500

    Widget RichText

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Text'),
+    return const MaterialApp(
+      home: MyHomePage(title: 'Flutter Text'),
     );
   }
 }
@@ -27,42 +22,25 @@ class MyHomePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    DefaultTextStyle defaultTextStyle = DefaultTextStyle.of(context);
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: Text(title),
       ),
-      body: const Center(
+      body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'Hola Mundo, dentro de una línea!!',
-              textAlign: TextAlign.center,
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Text.rich(
-              TextSpan(
-                text: 'Hola Mundo,',
-                children: <TextSpan>[
+            RichText(
+              text: TextSpan(
+                text: 'Hola ',
+                style: defaultTextStyle.style,
+                children: const <TextSpan>[
                   TextSpan(
-                      text: ' dentro de ',
-                      style: TextStyle(fontStyle: FontStyle.italic)),
-                  TextSpan(
-                      text: 'una línea!!',
+                      text: 'Mundo',
                       style: TextStyle(fontWeight: FontWeight.bold)),
-                ],
-              ),
-            ),
-            SelectionArea(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Text('Texto seleccionable'),
-                  SelectionContainer.disabled(
-                    child: Text('Texto no seleccionable'),
-                  ),
-                  Text('Texto seleccionable 2'),
+                  TextSpan(text: '!'),
                 ],
               ),
             ),
