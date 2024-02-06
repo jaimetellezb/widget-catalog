29803001d4bd52d2a01d1611b6e52d8bfa198f77
jaimetellezb <jaimetellez410@gmail.com>
2024-02-05 21:32:19 -0500

    widgets  assets - Image

--- lib/main.dart
+++ lib/main.dart
@@ -15,54 +15,58 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Assets'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
+class MyHomePage extends StatelessWidget {
   const MyHomePage({super.key, required this.title});
 
   final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
   Widget build(BuildContext context) {
+    const formatInvalid = 'error';
+    const url =
+        'https://www.crunchyroll.com/imgsrv/display/thumbnail/1200x675/catalog/crunchyroll/a249096c7812deb8c3c2c907173f3774.jpe';
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+            const Image(
+              height: 200,
+              width: 300,
+              image: AssetImage('assets/images/dash-fainting.gif'),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            Image.asset(
+              'assets/images/dashatars.png',
+              height: 200,
+              width: 300,
+            ),
+            Image.network(
+              url,
+              height: 200,
+              width: 300,
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Image.network(
+              errorBuilder: (context, error, stackTrace) => Text(
+                '$error',
+                style: const TextStyle(color: Colors.red),
+              ),
+              formatInvalid,
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
