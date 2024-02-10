c216b3074a9458c7da706aba45e6ce9652cb33ca
jaimetellezb <jaimetellez410@gmail.com>
2024-02-10 16:02:13 -0500

    Opacity and AnimatedOpacity Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const AnimatedMyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -10,59 +10,163 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      title: 'Flutter Opacity',
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Opacity(
+                opacity: 0.9,
+                child: Container(
+                  height: 150,
+                  width: 150,
+                  color: Colors.green,
+                  child: const Image(
+                    image: NetworkImage(
+                        'https://miro.medium.com/v2/resize:fit:1400/1*W1aGmyVwe5kKGuyTvzdUEg.png'),
+                  ),
+                ),
+              ),
+              Opacity(
+                opacity: 0.2,
+                child: Image.network(
+                  'https://ih1.redbubble.net/image.3266012404.3827/bg,f8f8f8-flat,750x,075,f-pad,750x1000,f8f8f8.jpg',
+                  height: 150,
+                  width: 150,
+                ),
+              ),
+              Opacity(
+                opacity: 0.5,
+                child: Container(
+                  height: 150,
+                  width: 150,
+                  color: Colors.red,
+                  child: Image.network('https://kili.aspix.it/Dash.png'),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class MyAppState extends StatefulWidget {
+  const MyAppState({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<MyAppState> createState() => _MyAppState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _MyAppState extends State<MyAppState> {
+  double opacityValue = 1.0;
 
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
+  void _changeOpacity() {
+    opacityValue -= 0.1;
+    if (opacityValue < 0.0) {
+      opacityValue = 1.0;
+    }
+
+    setState(() {});
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+    return MaterialApp(
+      title: 'Flutter Opacity',
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              Container(
+                color: Theme.of(context)
+                    .colorScheme
+                    .inversePrimary
+                    .withOpacity(opacityValue),
+                child: Opacity(
+                  opacity: opacityValue,
+                  child: Image.network(
+                    'https://kili.aspix.it/Dash.png',
+                    height: 300,
+                    width: 300,
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: _changeOpacity,
+                child: const Text('Opacidad'),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    );
+  }
+}
+
+class AnimatedMyApp extends StatefulWidget {
+  const AnimatedMyApp({super.key});
+
+  @override
+  State<AnimatedMyApp> createState() => _AnimatedMyAppState();
+}
+
+class _AnimatedMyAppState extends State<AnimatedMyApp> {
+  double opacityValue = 1.0;
+
+  void _changeOpacity() {
+    setState(() => opacityValue = opacityValue == 0 ? 1.0 : 0.0);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter AnimatedOpacity',
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              AnimatedOpacity(
+                duration: const Duration(seconds: 5),
+                opacity: opacityValue,
+                curve: Curves.fastOutSlowIn,
+                child: Container(
+                  color: Theme.of(context).colorScheme.inversePrimary,
+                  child: SizedBox(
+                    height: 300,
+                    width: 300,
+                    child: Image.network('https://kili.aspix.it/Dash.png'),
+                  ),
+                ),
+              ),
+              Container(
+                color: Theme.of(context)
+                    .colorScheme
+                    .inversePrimary
+                    .withOpacity(opacityValue),
+                child: SizedBox(
+                  height: 300,
+                  width: 300,
+                  child: Image.network(
+                    'https://kili.aspix.it/Dash.png',
+                    color: const Color.fromARGB(255, 62, 167, 202)
+                        .withOpacity(opacityValue),
+                  ),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: _changeOpacity,
+                child: opacityValue == 1.0
+                    ? const Text('Opacidad')
+                    : const Text('Color original'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
